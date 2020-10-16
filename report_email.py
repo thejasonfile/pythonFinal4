213e80a2904c3a8109cf325147e30f769205bc67
Jason Arnold <jason.arnold.00@gmail.com>
2020-10-16 13:20:54 -0400

    file location is working

--- report_email.py
+++ report_email.py
@@ -7,7 +7,7 @@ date = date.strftime("%m-%d-%Y")
 title = "Processed Update on {}".format(date)
 data = [[""], ["name:", "Apple"], ["weight:", "500lbs"], [""], ["name:", "Avocado"], ["weight:", "200lbs"]]
 
-reports.generate_report(title, data)
+reports.generate_report(title, data, "processed.pdf")
 
 if __name__ == "__main__":
     reports.generate_report
--- reports.py
+++ reports.py
@@ -5,8 +5,8 @@ from reportlab.platypus import Paragraph, Table, Spacer
 from reportlab.lib.styles import getSampleStyleSheet
 styles = getSampleStyleSheet()
 
-def generate_report(title, table_data):
-    report = SimpleDocTemplate("processed.pdf")
+def generate_report(title, table_data, file):
+    report = SimpleDocTemplate(file)
     report_title = Paragraph(title, styles["h1"])
     report_table = Table(data=table_data)
     report.build([report_title, report_table])
