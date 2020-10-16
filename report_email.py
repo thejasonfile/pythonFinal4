6b3b796feb8a7c2965ade55aa61bf93a9ffc2ef9
Jason Arnold <jason.arnold.00@gmail.com>
2020-10-16 13:34:09 -0400

    pdf being generated

--- report_email.py
+++ report_email.py
@@ -5,9 +5,21 @@ import reports, os, datetime
 date = datetime.datetime.now()
 date = date.strftime("%m-%d-%Y")
 title = "Processed Update on {}".format(date)
-data = [[""], ["name:", "Apple"], ["weight:", "500lbs"], [""], ["name:", "Avocado"], ["weight:", "200lbs"]]
 
-reports.generate_report(title, data, "processed.pdf")
+table_data = []
+files_path = ("supplier-data/descriptions/")
+files = os.listdir(files_path)
+
+for file in files:
+    file_data = []
+    with open(files_path + file, 'r') as file:
+        line1 = file.readline().rstrip('\n')
+        line2 = file.readline().rstrip('\n')
+        table_data.append([""])
+        table_data.append(["name:", line1])
+        table_data.append(["weight:", line2])
+
+reports.generate_report(title, table_data, "processed.pdf")
 
 if __name__ == "__main__":
     reports.generate_report
