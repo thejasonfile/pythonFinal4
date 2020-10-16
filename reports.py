26870641ed9f9082765ff877a429597908400311
Jason Arnold <jason.arnold.00@gmail.com>
2020-10-16 13:43:35 -0400

    added left align to pdf data

--- reports.py
+++ reports.py
@@ -8,5 +8,5 @@ styles = getSampleStyleSheet()
 def generate_report(title, table_data, file):
     report = SimpleDocTemplate(file)
     report_title = Paragraph(title, styles["h1"])
-    report_table = Table(data=table_data)
+    report_table = Table(data=table_data, hAlign="LEFT")
     report.build([report_title, report_table])
